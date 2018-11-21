68b3d5320c1052031a8f5da872ce7367447ada71
hiepndd <hiepdangnguyendai@gmail.com>
2018-11-21 17:16:12 +0700

    fix bug UI

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -2,8 +2,8 @@ class Pokemon {
   constructor(data) {
     this.id = data.id;
     this.name = data.name;
-    this.sprite = data.sprite.front_default;
-    this.type = data.type[0].type.name;
+    this.sprite = data.sprites.front_default;
+    this.type = data.types[0].type.name;
   }
 }
 export default Pokemon;
--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import PokeList from "./PokeList";
 import DetailView from "./DetailView";
 import Pokemon from "../Pokemon";
